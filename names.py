47f8cd8f970e5b5b9115c5d2112dab33ddfc7d70
k4m454k <k4m454k@gmail.com>
2019-10-27 20:52:23 +0300

    new names

---
+++ names.py
@@ -0,0 +1,14 @@
+from random import choice
+
+
+adjectives_m = ['']
+nouns_m = [
+    "пирожок", "кафель", "ёжик", "сандаль", "вафель", "хомяк", "индюшень", ""
+]
+
+adjectives_f = []
+nouns_f = []
+
+
+def get_name():
+    pass
